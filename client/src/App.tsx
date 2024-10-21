237987ffc73f77b4f85f28a9cce1ee7ab8c2961d
AresNeutron <arexneutron@gmail.com>
2024-10-21 18:49:13 -0300

    Uploading project to Github

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,32 @@
 import "./App.css";
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
-import Auth from "./pages/auth";
+import Auth from "./pages/auth/index";
 import { FinancialRecordProvider } from "./contexts/financial-record-context";
 import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 function App() {
   return (
     <Router>
+      <div className="navbar">
+        <Link to={"/"}>Dashboard</Link>
+        <Link to={"/auth"}>Authenticate</Link>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
       <div className="app-container">
-        <div className="navbar">
-          <Link to={'/'}>Dashboard</Link>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <FinancialRecordProvider>
-                <Dashboard />
-              </FinancialRecordProvider>
-            }
-          />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <FinancialRecordProvider>
+                  <Dashboard />
+                </FinancialRecordProvider>
+              }
+            />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
       </div>
     </Router>
   );
